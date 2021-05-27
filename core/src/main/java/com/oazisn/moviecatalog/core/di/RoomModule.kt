b85454c33f0fd00c52a4071028755136a740f048
azisnaufal <azis.naufal@gmail.com>
2021-05-27 22:00:39 +0700

    add sqlcipher

--- core/src/main/java/com/oazisn/moviecatalog/core/di/RoomModule.kt
+++ core/src/main/java/com/oazisn/moviecatalog/core/di/RoomModule.kt
@@ -4,11 +4,18 @@ import androidx.room.Room
 import com.oazisn.moviecatalog.core.data.local.AppDatabase
 import com.oazisn.moviecatalog.core.data.local.MovieDataSource
 import com.oazisn.moviecatalog.core.data.local.TvShowDataSource
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 val roomModule = module {
     single {
-        Room.databaseBuilder(get(), AppDatabase::class.java, AppDatabase.DATABASE_NAME).build()
+        val passphrase: ByteArray = SQLiteDatabase.getBytes("7Delapan789899#".toCharArray())
+        val factory = SupportFactory(passphrase)
+        Room.databaseBuilder(androidContext(), AppDatabase::class.java, AppDatabase.DATABASE_NAME)
+            .openHelperFactory(factory)
+            .build()
     }
 
     single { get<AppDatabase>().movieDao() }
